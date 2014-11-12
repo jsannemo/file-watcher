e3e11886c1773fc45611830349d9ca97fb90277a
Johan Sannemo <johan.sannemo@gmail.com>
2014-11-12 12:53:06 +0100

    Apparently vim deletes its files when it save. This caused some problems

--- watch.cpp
+++ watch.cpp
@@ -1,4 +1,6 @@
 #include <sys/inotify.h>
+#include <cstring>
+#include <map>
 #include <cstdlib>
 #include <vector>
 #include <unistd.h>
@@ -8,9 +10,21 @@
 
 using namespace std;
 
-constexpr int BUF_SIZE = sizeof(struct inotify_event) + NAME_MAX + 1;
+constexpr int BUF_SIZE = 2 * (sizeof(struct inotify_event) + NAME_MAX + 1);
+
+int inotifyFd;
 
 char buf[BUF_SIZE];
+
+map<int, string> watchDescriptors;
+
+void addDescriptor(const string& fileName){
+	int watchDescriptor = inotify_add_watch(inotifyFd, fileName.c_str(), IN_MODIFY);
+	if(watchDescriptor == -1){
+		perror("Could not initialize watch descriptor");
+	}
+	watchDescriptors[watchDescriptor] = fileName;
+}
 
 // If many eventes happen rapidly, we don't really care about it.
 bool readEvent(int fd){
@@ -22,6 +36,14 @@ bool readEvent(int fd){
 	if(readBytes == 0){
 		return false;
 	}
+	int idx = 0;
+	while(idx < readBytes){
+		inotify_event* pevent = (struct inotify_event *) &buf[idx];
+		if( (pevent->mask)&IN_IGNORED ){
+			addDescriptor(watchDescriptors[pevent->wd]);
+		}
+		idx += sizeof(pevent) + strlen(pevent->name) + 1;
+	}
 	return true;
 }
 
@@ -32,18 +54,14 @@ int main(int argc, const char** argv){
 	}
 	const char* command = argv[argc - 1];
 
-	int inotifyFd = inotify_init();
+	inotifyFd = inotify_init();
 	if(inotifyFd == -1){
 		perror("Could not initialize inotify");
 		return 2;
 	}
-	vector<int> watchDescriptors;
 	for(int fileIdx = 1; fileIdx < argc - 1; fileIdx++){
-		const char* fileName = argv[fileIdx];
-		int watchDescriptor = inotify_add_watch(inotifyFd, fileName, IN_MODIFY);
-		if(watchDescriptor == -1){
-			perror("Could not initialize watch descriptor");
-		}
+		string fileName(argv[fileIdx]);
+		addDescriptor(fileName);
 	}
 	while(readEvent(inotifyFd)){
 		system(command);
